165990936d8419be174cefa7fbf043de1d9a4015
Renan RodriguesDev <renanrodrigues7110@gmail.com>
2025-03-07 09:39:18 -0300

    feat: fix to css and create sidebar joined slidebar

--- tarefas/src/App.vue
+++ tarefas/src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="container">
-    <ImageSlider />
+    <SideBar class="side" />
+    <ImageSlider class="slider" />
   </div>
 </template>
 
 <script>
 import ImageSlider from './components/Slider.vue';
+import SideBar from './components/SideBar.vue'
 export default {
-  components: { ImageSlider }
+  components: { ImageSlider, SideBar }
 }
 </script>
 
 <style>
 .container {
-  background-color: #f3f3;
+  display: flex;
+  min-height: 100vh;
+  width: 100%;
+  gap: 20px;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.side {
+  align-content: center;
+
+}
+
+.slider {
+  align-content: center;
+  margin: 5% auto;
+  background-color: #f89dff;
 }
 </style>
---
+++ tarefas/src/components/SideBar.vue
@@ -0,0 +1,110 @@
+<script>
+export default {
+    name: 'SideBar',
+    data() {
+        return {
+            icons: [
+                { name: 'home', link: '/', img: 'fas fa-house-chimney' },
+                { name: 'docs', link: '/docs', img: 'fas fa-book' },
+                { name: 'help', link: '/help', img: 'fas fa-person' }
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <nav class="sidebar">
+        <div class="container">
+            <div class="logo">
+                <a href=""></a>
+            </div>
+
+            <ul class="side-links">
+                <li v-for="(icon, idx) in icons" :key="idx">
+                    <a :href="icon.link"><i :class="icon.img">{{ icon.name }}</i></a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+</template>
+<style scoped>
+.sidebar {
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    width: 80px;
+    /* Aumentei um pouco a largura */
+    background-color: #3a003e;
+    padding: 10px 0;
+    transition: width 0.3s ease;
+}
+
+.container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    /* Garante largura total */
+}
+
+.logo {
+    width: 40px;
+    height: 40px;
+    margin-bottom: 30px;
+}
+
+.side-links {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.side-links li {
+    width: 100%;
+    padding: 10px 0;
+    text-align: center;
+}
+
+.side-links a {
+    color: #ffffff;
+    text-decoration: none;
+    width: 100%;
+    /* Garante largura total */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 5px 0;
+}
+
+.side-links i {
+    font-size: 1.2rem;
+    /* Reduzi um pouco o tamanho */
+    margin-bottom: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    /* Garante largura total */
+}
+
+.side-links a:hover {
+    color: #ef7afc;
+}
+
+/* Nome do ícone */
+.side-links i::after {
+    content: attr(data-name);
+    /* Mudança para usar data-name ao invés de class */
+    font-size: 0.7rem;
+    margin-top: 5px;
+    font-family: sans-serif;
+    /* Adiciona uma fonte consistente */
+}
+</style>
